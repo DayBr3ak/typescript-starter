31662c491df3beef69df4085a3de4c1f9a5244d5
Benjamin Groeneveld <bgroeneveld@sifam.onmicrosoft.com>
2018-08-27 12:57:40 +0200

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { asyncABC } from "./lib/async";
-console.log('a, b')
-asyncABC().then(console.log)
+import express from 'express'
+import { ApiController } from './controllers';
 
+const app: express.Application = express();
+const port: string = process.env.PORT || '3000';
 
+app.use('/api', ApiController)
 
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});
